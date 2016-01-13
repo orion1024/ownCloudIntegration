726d74f9ef58f48fb6dcc3fcf836d8ca4044179d
orion1024 <orion1024+github@use.startmail.com>
2016-01-13 23:46:07 +0100

    1st functional building script
    tested with win7

--- build-client/build-client.bat
+++ build-client/build-client.bat
@@ -1,8 +1,33 @@
 @echo off
+setlocal
 
-cd E:\client-build
+rem -------------------------------------------------
+rem 	Build owncloud client 
+rem Author : orion1024
+rem Date : 2016
+rem -------------------------------------------------
 
-cmake -G "MinGW Makefiles" ../jenkins/client
+set BUILD_DIR=E:\client-build
+
+rem Setting source dir
+IF /I "%2"=="" (
+	set SRC_DIR=%WORKSPACE%
+) ELSE (
+	set SRC_DIR=%2
+)
+	
+IF NOT EXIST "%SRC_DIR%" (
+	echo Sources not found : "%SRC_DIR%" does not exists
+	exit 1
+)
+
+rem Purge old build data if any
+IF EXIST %BUILD_DIR%	del /F /Q %BUILD_DIR%
+mkdir %BUILD_DIR%
+cd %BUILD_DIR%
+
+rem Building it
+cmake -G "MinGW Makefiles" "%SRC_DIR%"
 mingw32-make
 
-pause
+endlocal
